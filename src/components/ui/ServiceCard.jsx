c432acfeb984c0b3fab547f51e63adb1eef16120
akshayam-hdrss <axolotronhdrss@gmail.com>
2025-06-05 14:09:04 +0530

    updated service cards list

--- src/components/ui/ServiceCard.jsx
+++ src/components/ui/ServiceCard.jsx
@@ -1,34 +1,6 @@
-// import React from "react";
-// import Image from "next/image";
-// import Link from "next/link";
-// function ServiceCard({ name, url, slug }) {
-//   console.log(name, "Namef")
-//   console.log(url, "url")
-//   console.log(slug, "slug")
-//   console.log(slug.split("/"));
-//   let productName = slug.split("/")[1];
-//   console.log(productName.toLowerCase(), "line 10");
-//   return (
-//     <Link
-//       href={slug}
-//       className="bg-gradient-to-b m-auto from-[#FCEDED] to-[#F6F6F6] text-center flex flex-col justify-center items-center w-[100px] h-[100px] bg-white border-2 border-solid border-[#909090] rounded-2xl cursor-pointer z-30"
-//     >
-//       <img src={url} alt={name} width={40} height={40} />
-//       <p
-//         className="font-medium text-md capitalize mt-1"
-//         style={{ wordBreak: "break-word" }}
-//       >
-//         {name}
-//       </p>
-//     </Link>
-//   );
-// }
-
-// export default ServiceCard;
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { subscribeToServicesAndProducts } from "@/firebase/firestore/servicesProducts";
 
@@ -50,43 +22,30 @@ function ServiceCard({ name, url, slug }) {
     };
   }, []);
 
-  console.log(data, "Line 34");
+  const iconUrl = url
+    ? url
+    : "https://via.placeholder.com/100?text=No+Image"; // default icon
+
+  const targetSlug =
+    productName === "products"
+      ? slug
+      : data && data.length > 0
+      ? `${slug}/${data[0].id}`
+      : slug; // fallback if no nested data
 
   return (
-    <>
-      {productName === "products" ? (
-        <>
-          <Link
-            href={slug}
-            className="bg-gradient-to-b m-auto from-[#FCEDED] to-[#F6F6F6] text-center flex flex-col justify-center items-center w-[100px] h-[100px] bg-white border-2 border-solid border-[#909090] rounded-2xl cursor-pointer z-30"
-          >
-            <img src={url} alt={name} width={40} height={40} />
-            <p
-              className="font-medium text-md capitalize mt-1"
-              style={{ wordBreak: "break-word" }}
-            >
-              {name}
-            </p>
-          </Link>
-        </>
-      ) : (
-        data &&
-        data?.length !== 0 && (
-          <Link
-            href={`${slug}/${data[0]?.id}`}
-            className="bg-gradient-to-b m-auto from-[#FCEDED] to-[#F6F6F6] text-center flex flex-col justify-center items-center w-[100px] h-[100px] bg-white border-2 border-solid border-[#909090] rounded-2xl cursor-pointer z-30"
-          >
-            <img src={url} alt={name} width={40} height={40} />
-            <p
-              className="font-medium text-md capitalize mt-1"
-              style={{ wordBreak: "break-word" }}
-            >
-              {name}
-            </p>
-          </Link>
-        )
-      )}
-    </>
+    <Link
+      href={targetSlug}
+      className="bg-gradient-to-b m-auto from-[#FCEDED] to-[#F6F6F6] text-center flex flex-col justify-center items-center w-[100px] h-[100px] bg-white border-2 border-solid border-[#909090] rounded-2xl cursor-pointer z-30"
+    >
+      <img src={iconUrl} alt={name} width={40} height={40} />
+      <p
+        className="font-medium text-md capitalize mt-1"
+        style={{ wordBreak: "break-word" }}
+      >
+        {name || "No Name"}
+      </p>
+    </Link>
   );
 }
 
